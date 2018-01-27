bb10036c2bb7e05367acbee7b8e305931cae9de0
Takashi Numao <takashinumao@gmail.com>
2018-01-27 22:44:28 +0900

    Add debug tool bar

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list')
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (
+        url(r'^your_app/', include(debug_toolbar.urls)),
+    )
